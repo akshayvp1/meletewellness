6e12c463f24f7d23c074dddf3101ba72fbfb4655
akshayvp1 <akshayachu2427@gmail.com>
2025-07-16 19:29:49 +0530

    fix: changes in eslint

---
+++ frontend/.eslintrc.js
@@ -0,0 +1,11 @@
+/** @type {import('eslint').Linter.Config} */
+module.exports = {
+  root: true,
+  extends: ['next', 'next/core-web-vitals', 'eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+  rules: {
+    '@typescript-eslint/no-unused-vars': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    'react/no-unescaped-entities': 'off',
+    '@next/next/no-img-element': 'off'
+  }
+};
