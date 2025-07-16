54a67ceb6631c65e843253602f1eec9e3a742c31
akshayvp1 <akshayachu2427@gmail.com>
2025-07-16 19:20:49 +0530

    fix: changes added in nextconfig

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -8,16 +8,33 @@
 
 
 
-module.exports = {
-  async rewrites() {
-    return [
-      {
-        source: '/api/:path*',
-        destination: 'http://localhost:4040/api/:path*',
-        images: {
-    unoptimized: true, // disables sharp optimization
+// module.exports = {
+//   async rewrites() {
+//     return [
+//       {
+//         source: '/api/:path*',
+//         destination: 'http://localhost:4040/api/:path*',
+ 
+//       },
+//     ];
+//   },
+// };
+
+
+
+
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    unoptimized: true // ✅ This goes here
   },
-      },
-    ];
-  },
-};
+  rewrites: async () => [
+    {
+      source: '/api/:path*',
+      destination: 'http://localhost:4040/api/:path*'
+    }
+  ]
+}
+
+module.exports = nextConfig;
