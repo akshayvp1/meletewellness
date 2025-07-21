2563f5898e3068f5d0666c809d6a07c1d3a64e71
akshayvp1 <akshayachu2427@gmail.com>
2025-07-21 18:40:23 +0530

    fix: error in controller

--- backend/src/routes/userRouter.ts
+++ backend/src/routes/userRouter.ts
@@ -10,6 +10,6 @@ const adminAuthController = container.resolve(AdminAuthController);
 user.get("/front-counsellors", adminAuthController.frontGetCounsellors);
 user.post("/google-login",adminAuthController.GoogleLogin)
 user.get('/check',userAuthenticate,adminAuthController.Check);
-user.post("/logout", userAuthenticate, adminAuthController.logout);
+user.post("/logout", adminAuthController.logout);
 
 export default user
